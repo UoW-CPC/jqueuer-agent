8df407d9ca1205635efca17fc81a3e33528944b2
jaydesl <jay.deslauriers@gmail.com>
2019-06-21 20:36:14 +0100

    Update parameters file

--- parameters.py
+++ parameters.py
@@ -1,48 +1,67 @@
+import os
+
 import redis
-JOB_QUEUE_PREFIX 	= 	'jqueue_service_'
-broker_protocol		= 	'pyamqp'
-broker_username 	= 	'admin'
-broker_password 	= 	'mypass'
-broker_server		=	'rabbit' # This value should be changed to the jqueuer_server Ip address 
-broker_port 		= 	5672
+from datadog import initialize
+from datadog import statsd
+
+# Job Queue Prefix
+JOB_QUEUE_PREFIX = "jqueue_service_"
+
+# Backend configuration - Rabbitmq
+broker_protocol = "pyamqp"
+broker_username = os.getenv("RABBIT_USER", "admin")
+broker_password = os.getenv("RABBIT_PASS", "mypass")
+broker_server = os.getenv("RABBIT_SERVER", "jqueuer-rabbit")
+broker_port = os.getenv("RABBIT_PORT", 5672)
+
 
 def broker():
-	broker 	= 	broker_protocol + '://' + broker_username 
-	if (broker_password != ''):
-		broker 	= broker + ':' + broker_password
-	broker 	= 	broker + '@' + broker_server + ':' + str(broker_port) + '//'
-	return broker
+    broker = broker_protocol + "://" + broker_username
+    if broker_password != "":
+        broker = broker + ":" + broker_password
+    broker = broker + "@" + broker_server + ":" + str(broker_port) + "//"
+    return broker
 
-# Redis backend configuration
-backend_protocol 			= 	'redis'
-backend_server	 			=	'redis' # This value should be changed to the jqueuer_server Ip address 
-backend_port     			=	6379
-backend_db		 			=	0
-backend_experiment_db_id	=	10
 
-backend_experiment_db = redis.StrictRedis(
-	host=backend_server, 
-	port=backend_port, 
-	db=backend_experiment_db_id, 
-	charset="utf-8", 
-	decode_responses=True
-	)
- 	
+# Redis Backend configuration
+backend_protocol = "redis"
+backend_server = os.getenv("REDIS_SERVER", "jqueuer-redis")
+backend_password = os.getenv("REDIS_PASS", "mypass")
+backend_port = os.getenv("REDIS_PORT", 6379)
+backend_db = 0
+backend_experiment_db_id = 10
+
+backend_experiment_db = redis.Redis(
+    host=backend_server,
+    port=backend_port,
+    password=backend_password,
+    db=backend_experiment_db_id,
+    charset="utf-8",
+    decode_responses=True,
+)
+
 def backend(db):
-	backend = backend_protocol + '://' + backend_server + ':' + str(backend_port) + '/' + str(db)
-	return backend
-
+    backend = (
+        backend_protocol
+        + "://:"
+        + backend_password
+        + "@"
+        + backend_server
+        + ":"
+        + str(backend_port)
+        + "/"
+        + str(db)
+    )
+    return backend
 
 
 # Prometheus exporer configuration
-STATSD_SERVER 	= 'statsd' # This value should be changed to the micado_master Ip address 
-STATSD_PORT		= 9125
+STATSD_SERVER = os.getenv("STATSD_SERVER", "jqueuer-statsd")
+STATSD_PORT = os.getenv("STATSD_PORT", 9125)
 STATSD_OPTIONS = {
-    'api_key':'jqueuer_api_key',
-    'app_key':'jqueuer_app_key',
-    'statsd_host': STATSD_SERVER,
-    'statsd_port': STATSD_PORT
+    "api_key": "jqueuer_api_key",
+    "app_key": "jqueuer_app_key",
+    "statsd_host": STATSD_SERVER,
+    "statsd_port": STATSD_PORT,
 }
-from datadog import initialize
 initialize(**STATSD_OPTIONS)
-from datadog import statsd
