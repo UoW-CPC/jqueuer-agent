603ccd5397d6087904076c4e5f033d5f948d877b
jaydesl <jay.deslauriers@gmail.com>
2019-06-24 11:42:20 +0100

    Clean up container search

--- jqueuer_agent.py
+++ jqueuer_agent.py
@@ -45,75 +45,70 @@ def start(node_id):
 
     while True:
         current_update += 1
-        try:
-            # Loop over the containers list
-            for container in client.containers.list():
-                container_obj = {}
+        # Loop over the containers list
+        for container in client.containers.list():
+            container_obj = {}
+            try:
+                container_state_running = container.attrs["State"]["Running"]
+                if not container_state_running:
+                    continue
+                # Get container ID
+                container_long_id = container.id
+
+                # Get container's service name
+                container_service_name = container.labels.get(
+                    "com.docker.swarm.service.name"
+                ) or container.labels.get("io.kubernetes.container.name") or container.name
+
+                # Check if the service belongs to an experiment
+                if not backend_experiment_db.exists(container_service_name):
+                    continue
+
+                # Get the experiment id
+                [experiment_id] = (
+                    backend_experiment_db.hmget(container_service_name, 'experiment_id')
+                )
+                # Check if the container has been already added, if yes, update the current_update
+                if container_long_id in container_list:
+                    container_list[container_long_id][
+                        "current_update"
+                    ] = current_update
+                    continue
+
+                container_obj = {
+                    "id_long": container_long_id,
+                    "name": container.attrs["Name"],
+                    "service_name": container_service_name,
+                    "task_id": container.labels.get("com.docker.swarm.task.id")
+                    or container.labels.get("io.kubernetes.pod.uid"),
+                    "task_name": container.labels.get("com.docker.swarm.task.name")
+                    or container.labels.get("io.kubernetes.pod.name"),
+                    "hostname": container.attrs["Config"]["Hostname"],
+                    "ip_address": "",
+                    "created": container.attrs["Created"],
+                    "started": container.attrs["State"]["StartedAt"],
+                    "experiment_id": experiment_id,
+                    "current_update": current_update,
+                }
+                container_obj["ip_address"] = (
+                    container.attrs["NetworkSettings"]["Networks"]
+                    .get("bridge", {})
+                    .get("IPAddress")
+                )
+
                 try:
-                    # Get container ID
-                    container_long_id = container.attrs["Id"]
-
-                    # Get container's service name
-                    config_labels = container.attrs["Config"]["Labels"]
-                    container_service_name = config_labels.get(
-                        "com.docker.swarm.service.name"
-                    ) or config_labels.get("io.kubernetes.container.name")
-
-                    container_state_running = container.attrs["State"]["Running"]
-                    if container_state_running != True:
-                        continue
-
-                    # Check if the service belongs to an experiment
-                    if not backend_experiment_db.exists(container_service_name):
-                        continue
-
-                    # Get the experiment data
-                    experiment = ast.literal_eval(
-                        backend_experiment_db.get(container_service_name)
+                    # Start a new thread to control this container
+                    job_worker_thread = Thread(
+                        target=worker, args=(container_obj, node_id)
                     )
+                    job_worker_thread.start()
+                    container_list[container_long_id] = container_obj
 
-                    # Check if the container has been already added, if yes, update the current_update
-                    if container_long_id in container_list:
-                        container_list[container_long_id][
-                            "current_update"
-                        ] = current_update
-                        continue
-
-                    container_obj = {
-                        "id_long": container_long_id,
-                        "name": container.attrs["Name"],
-                        "service_name": container_service_name,
-                        "task_id": config_labels.get("com.docker.swarm.task.id")
-                        or config_labels.get("io.kubernetes.pod.uid"),
-                        "task_name": config_labels.get("com.docker.swarm.task.name")
-                        or config_labels.get("io.kubernetes.pod.name"),
-                        "hostname": container.attrs["Config"]["Hostname"],
-                        "ip_address": "",
-                        "created": container.attrs["Created"],
-                        "started": container.attrs["State"]["StartedAt"],
-                        "experiment_id": experiment["experiment_id"],
-                        "current_update": current_update,
-                    }
-                    container_obj["ip_address"] = (
-                        container.attrs["NetworkSettings"]["Networks"]
-                        .get("bridge", {})
-                        .get("IPAddress")
-                    )
-
-                    try:
-
-                        # Start a new thread to control this container
-                        job_worker_thread = Thread(
-                            target=worker, args=(container_obj, node_id)
-                        )
-                        job_worker_thread.start()
-                        container_list[container_long_id] = container_obj
-
-                    except Exception as e:
-                        pass
                 except Exception as e:
                     pass
-
+            except Exception as e:
+                pass
+        try:
             # Containers that should be deleted from the list because they aren't alive
             trash = []
 
@@ -125,9 +120,10 @@ def start(node_id):
                     trash.append(container_id_temp)
             for x in trash:
                 del container_list[x]
-
         except Exception as e:
-            time.sleep(2.5)
+            time.sleep(5)
+            
+        time.sleep(2.5)
 
 
 if __name__ == "__main__":
