f5fff18428bce0abfd701ab3b4eccb405332e99f
amjad_ullah <amjad.qau@gmail.com>
2019-11-15 10:05:12 +0000

    fix(env variable): env variable data type issue

--- parameters.py
+++ parameters.py
@@ -13,7 +13,7 @@ broker_server = os.getenv("RABBIT_SERVER", "jqueuer-rabbit")
 broker_port = os.getenv("RABBIT_PORT", 5672)
 
 # Jqueuer job max retries configuration
-jqueuer_job_max_retries  = os.getenv("JQUEUER_JOB_MAX_RETRIES", 3)
+jqueuer_job_max_retries  = int(os.getenv("JQUEUER_JOB_MAX_RETRIES", "3"))
 
 # JQueuer Manager Service configuration
 JQUEUER_SERVER = os.getenv("JQUEUER_MANAGER_SERVICE_NAME", "jqueuer-manager")
